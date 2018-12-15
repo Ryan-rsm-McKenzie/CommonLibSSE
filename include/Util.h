b0d485b2cd938140b15cae20499b6b756e0a1eca
SniffleMan <ryan__mckenzie@hotmail.com>
2018-12-14 16:30:08 -0800

    Update Util.h

--- include/Util.h
+++ include/Util.h
@@ -1,53 +1,53 @@
 #pragma once
 
-#include <type_traits>  // underlying_type_t
+#include <type_traits>  // underlying_type_t, is_enum
 
 
-template <typename EnumType>
-constexpr auto to_underlying(EnumType a_typedEnum) noexcept
+template <typename Enum>
+constexpr auto to_underlying(Enum a_val) noexcept
 {
-	return static_cast<std::underlying_type_t<EnumType>>(a_typedEnum);
+	return static_cast<std::underlying_type_t<Enum>>(a_val);
 }
 
 
-//unary ~operator    
+// unary ~operator    
 template <typename Enum, typename std::enable_if_t<std::is_enum<Enum>::value, int> = 0>
-constexpr inline Enum& operator~ (Enum& val)
+constexpr inline Enum operator ~(Enum a_val)
 {
-	val = static_cast<Enum>(~static_cast<std::underlying_type_t<Enum>>(val));
-	return val;
+	a_val = static_cast<Enum>(~static_cast<std::underlying_type_t<Enum>>(a_val));
+	return a_val;
 }
 
 
 // & operator
 template <typename Enum, typename std::enable_if_t<std::is_enum<Enum>::value, int> = 0>
-constexpr inline Enum operator& (Enum lhs, Enum rhs)
+constexpr inline Enum operator &(Enum a_lhs, Enum a_rhs)
 {
-	return static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(lhs) & static_cast<std::underlying_type_t<Enum>>(rhs));
+	return static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(a_lhs) & static_cast<std::underlying_type_t<Enum>>(a_rhs));
 }
 
 
 // &= operator
 template <typename Enum, typename std::enable_if_t<std::is_enum<Enum>::value, int> = 0>
-constexpr inline Enum operator&= (Enum& lhs, Enum rhs)
+constexpr inline Enum operator &=(Enum& a_lhs, Enum a_rhs)
 {
-	lhs = static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(lhs) & static_cast<std::underlying_type_t<Enum>>(rhs));
-	return lhs;
+	a_lhs = static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(a_lhs) & static_cast<std::underlying_type_t<Enum>>(a_rhs));
+	return a_lhs;
 }
 
 
-//| operator
+// | operator
 template <typename Enum, typename std::enable_if_t<std::is_enum<Enum>::value, int> = 0>
-constexpr inline Enum operator| (Enum lhs, Enum rhs)
+constexpr inline Enum operator |(Enum a_lhs, Enum a_rhs)
 {
-	return static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(lhs) | static_cast<std::underlying_type_t<Enum>>(rhs));
+	return static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(a_lhs) | static_cast<std::underlying_type_t<Enum>>(a_rhs));
 }
 
 
-//|= operator
+// |= operator
 template <typename Enum, typename std::enable_if_t<std::is_enum<Enum>::value, int> = 0>
-constexpr inline Enum& operator|= (Enum& lhs, Enum rhs)
+constexpr inline Enum& operator |=(Enum& a_lhs, Enum a_rhs)
 {
-	lhs = static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(lhs) | static_cast<std::underlying_type_t<Enum>>(rhs));
-	return lhs;
+	a_lhs = static_cast<Enum>(static_cast<std::underlying_type_t<Enum>>(a_lhs) | static_cast<std::underlying_type_t<Enum>>(a_rhs));
+	return a_lhs;
 }
