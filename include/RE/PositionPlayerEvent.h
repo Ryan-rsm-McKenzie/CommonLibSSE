1e67511697040e95531a18a4c434d970db4349ee
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-07-09 16:53:37 -0700

    Update PositionPlayerEvent.h

--- include/RE/PositionPlayerEvent.h
+++ include/RE/PositionPlayerEvent.h
@@ -3,8 +3,21 @@
 
 namespace RE
 {
+	// related to cell transitions
 	struct PositionPlayerEvent
 	{
 	public:
+		enum class Type : UInt32
+		{
+			kStarted = 0,
+			kFastTravelUnk1 = 1,
+			kFastTravelUnk2 = 2,
+			kArrived = 3,
+			kFinished = 4
+		};
+
+
+		Type type;	// 0
 	};
+	STATIC_ASSERT(sizeof(PositionPlayerEvent) == 0x4);
 }
