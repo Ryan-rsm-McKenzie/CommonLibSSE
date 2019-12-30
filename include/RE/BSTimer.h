6f1a813488853cdc5a628532c6d87b232dac6db2
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-12-30 13:51:59 -0800

    Update BSTimer.h

--- include/RE/BSTimer.h
+++ include/RE/BSTimer.h
@@ -6,27 +6,23 @@ namespace RE
 	class BSTimer
 	{
 	public:
-		UInt64	unk00;		// 00
-
-		UInt32	freqLow;	// 08 (= Frequency.LowPart)
-		UInt32	freqHigh;	// 0C (= Frequency.HighPart)
-
-		UInt64	unk10;		// 10
-		float	unk18;		// 18
-		float	unk1C;		// 1C
-		UInt32	unk20;		// 20
-		UInt32	unk24;		// 24
-		float	unk28;		// 28
-
-		UInt32	unk2C;		// 2C
-		UInt32	unk30;		// 30
-
-		UInt32	unk34;		// 34
-		UInt8	unk38;		// 38
-		UInt8	unk39;		// 39
-		UInt8	unk3A;		// 3A
-		UInt8	pad3B;		// 3B
-		UInt32	pad3C;		// 3C
+		UInt64	unk00;							// 00
+		UInt32	lastPerformanceCount;			// 08
+		float	clamp;							// 10
+		float	clampRemainder;					// 14
+		float	delta;							// 18
+		float	realTimeDelta;					// 1C
+		UInt32	unk20;							// 20
+		UInt32	unk24;							// 24
+		float	unk28;							// 28
+		UInt32	unk2C;							// 2C
+		UInt32	unk30;							// 30
+		UInt32	unk34;							// 34
+		UInt8	unk38;							// 38
+		UInt8	unk39;							// 39
+		bool	useGlobalTimeMultiplierTarget;	// 3A
+		UInt8	pad3B;							// 3B
+		UInt32	pad3C;							// 3C
 	};
 	STATIC_ASSERT(sizeof(BSTimer) == 0x40);
 }
