3fc36ac57542af436334e9e30c9785be96a1d562
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2020-09-26 16:48:41 +0800

    Update Actor.h
    Add ResetAI member function.

--- include/RE/FormComponents/TESForm/TESObjectREFR/Actor/Actor.h
+++ include/RE/FormComponents/TESForm/TESObjectREFR/Actor/Actor.h
@@ -533,6 +533,7 @@ namespace RE
 		void						 RemoveExtraArrows3D();
 		bool						 RemoveSpell(SpellItem* a_spell);
 		std::int32_t				 RequestDetectionLevel(Actor* a_target, DETECTION_PRIORITY a_priority = DETECTION_PRIORITY::kNormal);
+		void					  	 ResetAI(UInt32 a_arg1 = 0, UInt32 a_arg2 = 1);
 		void						 StealAlarm(TESObjectREFR* a_ref, TESForm* a_object, std::int32_t a_num, std::int32_t a_total, TESForm* a_owner, bool a_allowWarning);
 		void						 SwitchRace(TESRace* a_race, bool a_player);
 		void						 UpdateArmorAbility(TESForm* a_armor, ExtraDataList* a_extraData);
