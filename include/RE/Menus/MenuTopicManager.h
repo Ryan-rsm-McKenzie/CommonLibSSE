c77564942134ff860f3cb067e2e071d3a38e2340
Vermunds <33235701+Vermunds@users.noreply.github.com>
2020-12-09 19:29:30 +0100

    Update MenuTopicManager.h

--- include/RE/Menus/MenuTopicManager.h
+++ include/RE/Menus/MenuTopicManager.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include "RE/BSCore/BSFixedString.h"
 #include "RE/BSCore/BSString.h"
 #include "RE/BSCore/BSTEvent.h"
 #include "RE/BSCore/BSTList.h"
 #include "RE/BSCore/BSTSingleton.h"
 
+#include "RE/FormComponents/Components/BGSSceneAction/BGSSceneAction.h"
 
 namespace RE
 {
@@ -30,32 +32,42 @@ namespace RE
 		struct Dialogue
 		{
 		public:
-			struct Data
+			struct ResponseData
 			{
 			public:
 				// members
-				BSSimpleList<BSString*> responses;	  // 00
-				TESQuest*				parentQuest;  // 10
-				TESTopicInfo*			topicInfo;	  // 18
-				TESTopic*				topic;		  // 20
-				Data*					unk28;		  // 28
-				std::uint8_t			unk30;		  // 30
-				bool					neverSaid;	  // 31
-				std::uint16_t			unk32;		  // 32
-				std::uint32_t			unk34;		  // 34
-				TESTopic*				unk38;		  // 38
+				BSString					responseText;	 // 00
+				BGSSceneAction::EmotionType emotionType;	 // 10
+				std::uint32_t				emotionValue;	 // 14
+				BSFixedString				voiceFilePath;	 // 18
+				std::uint64_t				unk20;			 // 20
+				std::uint64_t				unk28;			 // 28
+				std::uint64_t				unk30;			 // 30
+				bool						useEmotionAnim;	 // 38
+				bool						hasLipFile;		 // 39
+				std::uint16_t				pad3A;			 // 3A
+				std::uint32_t				pad3C;			 // 3C
 			};
-			static_assert(sizeof(Data) == 0x40);
+			static_assert(sizeof(ResponseData) == 0x40);
 
 
 			// members
-			BSString	  topicText;  // 00
-			bool		  unk10;	  // 10
-			bool		  unk11;	  // 11
-			bool		  unk12;	  // 12 - data.topic->formID == 0xFD || data.topic->formID == 0x118
-			std::uint8_t  unk13;	  // 13
-			std::uint32_t unk14;	  // 14
-			Data		  data;		  // 18
+			BSString					 topicText;		   // 00
+			bool						 unk10;			   // 10
+			bool						 unk11;			   // 11
+			bool						 unk12;			   // 12 - data.topic->formID == 0xFD || data.topic->formID == 0x118
+			std::uint8_t				 unk13;			   // 13
+			std::uint32_t				 unk14;			   // 14
+			BSSimpleList<ResponseData*>	 responses;		   // 18
+			TESQuest*					 parentQuest;	   // 28
+			TESTopicInfo*				 parentTopicInfo;  // 30
+			TESTopic*					 parentTopic;	   // 38
+			BSSimpleList<ResponseData*>* unk40;			   // 40
+			std::uint8_t				 unk48;			   // 48
+			bool						 neverSaid;		   // 49
+			std::uint16_t				 unk4A;			   // 4A
+			std::uint32_t				 unk4C;			   // 4C
+			TESTopic*					 unk50;			   // 50
 		};
 		static_assert(sizeof(Dialogue) == 0x58);
 
@@ -73,32 +85,33 @@ namespace RE
 
 
 		// members
-		BSSimpleList<Dialogue*>*	 unk18;				  // 18
-		BSSimpleList<Dialogue*>*	 dialogueList;		  // 20
-		std::uint64_t				 unk28;				  // 28
-		TESTopicInfo*				 unk30;				  // 30
-		Dialogue*					 currentDialogue;	  // 38
-		CRITICAL_SECTION			 criticalSection;	  // 40
-		RefHandle					 dialogueTarget;	  // 68
-		RefHandle					 lastDialogueTarget;  // 6C - used if the dialogue menu was closed but the NPC is still talking
-		TESTopicInfo*				 currentTopicInfo;	  // 70 - only valid when the NPC is talking
-		TESTopicInfo*				 lastTopicInfo;		  // 78 - used if the dialogue menu was closed but the NPC is still talking
-		BSTArray<BGSDialogueBranch*> blockingBranches;	  // 80
-		BSTArray<BGSDialogueBranch*> topLevelBranches;	  // 98
-		bool						 unkB0;				  // B0
-		bool						 unkB1;				  // B1
-		bool						 unkB2;				  // B2
-		bool						 unkB3;				  // B3
-		bool						 unkB4;				  // B4
-		bool						 unkB5;				  // B5
-		bool						 unkB6;				  // B6
-		bool						 unkB7;				  // B7
-		bool						 unkB8;				  // B8
-		bool						 unkB9;				  // B9
-		bool						 unkBA;				  // BA
-		bool						 unkBB;				  // BB
-		std::uint16_t				 padBC;				  // BC
-		BSTArray<TESTopic*>			 unkC0;				  // C0
+		BSSimpleList<Dialogue*>*	 selectedResponseNode;	// 18
+		BSSimpleList<Dialogue*>*	 dialogueList;			// 20
+		std::uint64_t				 unk28;					// 28
+		TESTopicInfo*				 rootTopicInfo;			// 30
+		Dialogue*					 lastSelectedDialogue;	// 38
+		CRITICAL_SECTION			 criticalSection;		// 40
+		RefHandle					 dialogueSpeaker;		// 68
+		RefHandle					 lastDialogueSpeaker;	// 6C - used if the dialogue menu was closed but the NPC is still talking
+		TESTopicInfo*				 currentTopicInfo;		// 70 - only valid when the NPC is talking
+		TESTopicInfo*				 lastTopicInfo;			// 78 - used if the dialogue menu was closed but the NPC is still talking
+		BSTArray<BGSDialogueBranch*> blockingBranches;		// 80
+		BSTArray<BGSDialogueBranch*> topLevelBranches;		// 98
+		bool						 unkB0;					// B0
+		bool						 unkB1;					// B1
+		bool						 unkB2;					// B2
+		bool						 unkB3;					// B3
+		bool						 unkB4;					// B4
+		bool						 unkB5;					// B5
+		bool						 unkB6;					// B6
+		bool						 unkB7;					// B7
+		bool						 unkB8;					// B8
+		bool						 unkB9;					// B9
+		bool						 unkBA;					// BA
+		bool						 unkBB;					// BB
+		std::uint16_t				 padBC;					// BC
+		BSTArray<TESTopic*>			 unkC0;					// C0
+		std::uint64_t				 unkD8;					// D8 - ???
 	};
-	static_assert(sizeof(MenuTopicManager) == 0xD8);
+	static_assert(sizeof(MenuTopicManager) == 0xE0);
 }
