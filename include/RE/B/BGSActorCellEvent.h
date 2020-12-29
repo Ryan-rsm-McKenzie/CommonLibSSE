0a4176d8e2ca89612034ffaf8fe5ed7afc72eaaf
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2020-12-30 00:28:31 +0800

    Create BGSActorCellEvent.h

---
+++ include/RE/B/BGSActorCellEvent.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace RE
+{
+	// related to cell transitions, trigger everytime when actor enter a new cell \ leave from a previous cell.
+	struct BGSActorCellEvent
+	{
+		enum class CellFlag : std::uint32_t
+		{
+			kFlags_Enter = 0,
+			kFlags_Leave = 1
+		};
+
+		std::uint32_t	refHandle;
+		FormID			cellID;
+		CellFlag	    flags;
+	};
+	static_assert(sizeof(BGSActorCellEvent) == 0xc);
+}
