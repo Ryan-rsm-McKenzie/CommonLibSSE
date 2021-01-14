4b5940da9ae381c1ca011cc7a6d43f2001984442
Vermunds <33235701+Vermunds@users.noreply.github.com>
2021-01-14 23:09:12 +0100

    Added EquipSpell and EquipShout (#63)
    * Added EquipSpell and EquipShout
    
    * Update ActorEquipManager.h

--- include/RE/A/ActorEquipManager.cpp
+++ include/RE/A/ActorEquipManager.cpp
@@ -15,6 +15,20 @@ namespace RE
 		return func(this, a_actor, a_object, a_extraData, a_count, a_slot, a_queueEquip, a_forceEquip, a_playSounds, a_applyNow);
 	}
 
+	void ActorEquipManager::EquipShout(Actor* a_actor, TESShout* a_shout)
+	{
+		using func_t = decltype(&ActorEquipManager::EquipShout);
+		REL::Relocation<func_t> func{ REL::ID(37941) };
+		return func(this, a_actor, a_shout);
+	}
+
+	void ActorEquipManager::EquipSpell(Actor* a_actor, SpellItem* a_spell, const BGSEquipSlot* a_slot)
+	{
+		using func_t = decltype(&ActorEquipManager::EquipSpell);
+		REL::Relocation<func_t> func{ REL::ID(37939) };
+		return func(this, a_actor, a_spell, a_slot);
+	}
+
 	bool ActorEquipManager::UnequipObject(Actor* a_actor, TESBoundObject* a_object, ExtraDataList* a_extraData, std::uint32_t a_count, const BGSEquipSlot* a_slot, bool a_queueEquip, bool a_forceEquip, bool a_playSounds, bool a_applyNow, const BGSEquipSlot* a_slotToReplace)
 	{
 		using func_t = decltype(&ActorEquipManager::UnequipObject);
