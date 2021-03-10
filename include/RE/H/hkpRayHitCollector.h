57c3359495c145c21e061929137c1fd508dcfb68
KernalsEgg <52105649+KernalsEgg@users.noreply.github.com>
2021-03-11 02:38:08 +1030

    2021-03-11 CommonLibSSE

--- include/RE/H/hkpRayHitCollector.h
+++ include/RE/H/hkpRayHitCollector.h
@@ -6,10 +6,10 @@ namespace RE
 	{
 	public:
 		hkpRayHitCollector() noexcept = default;
-		
+
 		hkpRayHitCollector(std::uintptr_t a_vtbl) noexcept :
 			vtbl(a_vtbl){};
-		
+
 		// members
 		std::uintptr_t vtbl{ 0 };					 // 0
 		float		   earlyOutHitFraction{ 1.0F };	 // 8
