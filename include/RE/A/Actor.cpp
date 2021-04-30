ccf73d543a5948ac6e515a805b7c61057b3fb333
max-su-2019 <54713650+max-su-2019@users.noreply.github.com>
2021-05-01 00:12:09 +0800

    Remove IsPowerAttacking

--- include/RE/A/Actor.cpp
+++ include/RE/A/Actor.cpp
@@ -420,21 +420,7 @@ namespace RE
 		return boolBits.all(BOOL_BITS::kPlayerTeammate);
 	}
 	
-
-	bool Actor::IsPowerAttacking() const
-	{
-		if (!this->currentProcess || !this->currentProcess->high)
-			return false;
-
-		auto attackData = this->currentProcess->high->attackData;
-
-		if (attackData)
-			return attackData->data.flags.any(RE::AttackData::AttackFlag::kPowerAttack);
-		else
-			return false;
-	}
-
-
+	
 	bool Actor::IsRunning() const
 	{
 		using func_t = decltype(&Actor::IsRunning);
