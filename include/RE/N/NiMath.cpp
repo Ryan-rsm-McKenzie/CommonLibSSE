b2532f4bab6ac2997ef8fb731cd7adc19e87c35d
noah the goodra <peterpan0413@live.com>
2021-06-20 22:32:24 -0500

    remove the casts and enforce float postfix

--- include/RE/N/NiMath.cpp
+++ include/RE/N/NiMath.cpp
@@ -9,8 +9,8 @@ namespace RE
 
 	float NiASin(float a_value)
 	{
-		if (-1.0 < a_value) {
-			if (a_value < 1.0) {
+		if (-1.0F < a_value) {
+			if (a_value < 1.0F) {
 				return std::asin(a_value);
 			} else {
 				return NI_HALF_PI;
@@ -22,50 +22,48 @@ namespace RE
 
 	float NiFastATan2(float a_y, float a_x)
 	{
-		if (a_x == 0.0 && a_y == 0.0) {
-			return 0.0;
+		if (a_x == 0.0F && a_y == 0.0F) {
+			return 0.0F;
 		}
 
-		float offset = 0.0;
+		float offset = 0.0F;
 		float z;
 		if (NiAbs(a_y) > NiAbs(a_x)) {
 			z = a_x / a_y;
-			if (z > 0.0) {
+			if (z > 0.0F) {
 				offset = NI_HALF_PI;
-			} else if (z < 0.0) {
+			} else if (z < 0.0F) {
 				offset = -NI_HALF_PI;
 			} else {
-				return (a_y > 0.0) ? NI_HALF_PI : -NI_HALF_PI;
+				return (a_y > 0.0F) ? NI_HALF_PI : -NI_HALF_PI;
 			}
 		} else {
 			z = a_y / a_x;
-			if (z == 0.0) {
-				return (a_x > 0.0) ? static_cast<float>(0.0) : NI_PI;
+			if (z == 0.0F) {
+				return (a_x > 0.0F) ? 0.0F : NI_PI;
 			}
 		}
 
 		float z2 = z * z;
 
 		float result;
-		result = static_cast<float>(0.0208351);
+		result = 0.0208351F;
 		result *= z2;
-		result -= static_cast<float>(0.0851330);
+		result -= 0.0851330F;
 		result *= z2;
-		result += static_cast<float>(0.1801410);
+		result += 0.1801410F;
 		result *= z2;
-		result -= static_cast<float>(0.3302995);
+		result -= 0.3302995F;
 		result *= z2;
-		result += static_cast<float>(0.9998660);
+		result += 0.9998660F;
 		result *= z;
 
-		if (offset) {
-			result = offset - result;
-		}
+		result = offset - result;
 
-		if (a_y < 0.0 && a_x < 0.0) {
+		if (a_y < 0.0F && a_x < 0.0F) {
 			result -= NI_PI;
 		}
-		if (a_y > 0.0 && a_x < 0.0) {
+		if (a_y > 0.0F && a_x < 0.0F) {
 			result += NI_PI;
 		}
 
