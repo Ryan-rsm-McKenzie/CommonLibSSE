da9c08d7f57975542f38f432bc9f104ab673f515
Qudix <17361645+Qudix@users.noreply.github.com>
2021-07-08 14:31:18 -0500

    IsLight & GetPartialIndex

--- include/RE/T/TESFile.h
+++ include/RE/T/TESFile.h
@@ -50,6 +50,8 @@ namespace RE
 		constexpr std::uint32_t GetCurrentSubRecordSize() const noexcept { return actualChunkSize; }
 		constexpr std::uint32_t GetCurrentChunkID() const noexcept { return currentchunkID; }
 		FormType                GetFormType();
+		constexpr std::uint32_t GetPartialIndex() const noexcept { return !IsLight() ? compileIndex : (0xFE000 | smallFileCompileIndex); };
+		constexpr bool          IsLight() const noexcept { return recordFlags.all(RecordFlag::kSmallFile); };
 		constexpr bool          IsLocalized() const noexcept { return recordFlags.all(RecordFlag::kDelocalized); }
 		void                    ReadData(void* a_buf, std::uint32_t a_size);
 		bool                    Seek(std::uint32_t a_offset);
