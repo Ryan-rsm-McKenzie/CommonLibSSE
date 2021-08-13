6a7c153c5477e23623e55a872b97e548e56b6b3b
Jordan Francis Moran-Meyers <69991101+jfm535@users.noreply.github.com>
2021-08-12 21:17:16 -0500

    Update NiNode.cpp
    reinterpret the pointer to a void* so that memset gets the type it wants

--- src/RE/N/NiNode.cpp
+++ src/RE/N/NiNode.cpp
@@ -7,7 +7,7 @@ namespace RE
 	NiNode* NiNode::Create(std::uint16_t a_arrBufLen)
 	{
 		auto node = malloc<NiNode>();
-		std::memset(node, 0, sizeof(NiNode));
+		std::memset(reinterpret_cast<void*>(node), 0, sizeof(NiNode));
 		node->Ctor(a_arrBufLen);
 		return node;
 	}
