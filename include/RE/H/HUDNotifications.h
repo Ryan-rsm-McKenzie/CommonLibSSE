ed81488f1d2ab28b1965808ec796653fc16f4caa
zax-ftw <zax-ftw@users.noreply.github.com>
2021-08-16 13:42:04 -0700

    Add HUDNotifications and Notification
    Co-Authored-By: Ryan <ryan__mckenzie@hotmail.com>

--- include/RE/H/HUDNotifications.h
+++ include/RE/H/HUDNotifications.h
@@ -1,40 +1,37 @@
 #pragma once
 
 #include "RE/B/BSFixedString.h"
+#include "RE/B/BSString.h"
 #include "RE/B/BSTArray.h"
 #include "RE/G/GFxValue.h"
 #include "RE/H/HUDObject.h"
 
 namespace RE
 {
+	class TESQuest;
+	class TESWordOfPower;
+
 	class HUDNotifications : public HUDObject
 	{
 	public:
 		inline static constexpr auto RTTI = RTTI_HUDNotifications;
 
-		struct UnkData1
+		struct Notification
 		{
-			struct UnkData2
-			{
-				void*         unk00;  // 00
-				std::uint64_t unk08;  // 08
-				void*         unk10;  // 10
-				std::uint64_t unk18;  // 18
-			};
-			static_assert(sizeof(UnkData2) == 0x20);
-
-			const char*        unk00;  // 00
-			std::uint64_t      unk08;  // 08
-			const char*        unk10;  // 10
-			std::uint64_t      unk18;  // 18
-			BSFixedString      unk20;  // 20
-			BSTArray<UnkData2> unk28;  // 28
-			std::uint64_t      unk40;  // 40
-			std::uint64_t      unk48;  // 48
-			std::uint64_t      unk50;  // 50
-			std::uint64_t      unk58;  // 58
+		public:
+			// members
+			BSString                text;              // 00
+			BSString                status;            // 10
+			BSFixedString           sound;             // 20
+			BSTArray<BSFixedString> objectives;        // 28
+			std::uint32_t           type{ 0 };         // 40
+			std::uint32_t           pad44{ 0 };        // 44
+			TESQuest*               quest{ nullptr };  // 48
+			TESWordOfPower*         word{ nullptr };   // 50
+			std::uint32_t           time{ 0 };         // 58 - gameTime + iObjectivesWaitTime
+			std::uint32_t           pad5C{ 0 };        // 5C
 		};
-		static_assert(sizeof(UnkData1) == 0x60);
+		static_assert(sizeof(Notification) == 0x60);
 
 		~HUDNotifications() override;  // 00
 
@@ -45,11 +42,11 @@ namespace RE
 		void CleanUp() override;                                       // 04
 
 		// members
-		BSTArray<UnkData1> unk28;                  // 28
-		GFxValue           questNotificationRoot;  // 40 - "HUDMovieBaseInstance.QuestUpdateBaseInstance"
-		GFxValue           objectives;             // 58 - "QuestNotification.ObjectivesA"
-		std::uint32_t      unk70;                  // 70
-		float              levelPoints;            // 74
+		BSTArray<Notification> queue;        // 28
+		GFxValue               root;         // 40 - "HUDMovieBaseInstance.QuestUpdateBaseInstance"
+		GFxValue               objectives;   // 58 - "QuestNotification.ObjectivesA"
+		std::uint32_t          time;         // 70 - last meter update time
+		float                  levelPoints;  // 74 - cached meter progress
 	};
 	static_assert(sizeof(HUDNotifications) == 0x78);
 }
