3fe3e375973942c395ca0338188de14556679616
powerof3 <32599957+powerof3@users.noreply.github.com>
2022-01-15 05:59:18 +0530

    update

--- include/RE/B/BSTempEffectGeometryDecal.h
+++ include/RE/B/BSTempEffectGeometryDecal.h
@@ -18,6 +18,7 @@ namespace RE
 	public:
 		inline static constexpr auto RTTI = RTTI_BSTempEffectGeometryDecal;
 		inline static constexpr auto Ni_RTTI = NiRTTI_BSTempEffectGeometryDecal;
+		inline static constexpr auto VTABLE = VTABLE_BSTempEffectGeometryDecal;
 		inline static constexpr auto TYPE = TEMP_EFFECT_TYPE::kGeometryDecal;
 
 		~BSTempEffectGeometryDecal() override;  // 00
