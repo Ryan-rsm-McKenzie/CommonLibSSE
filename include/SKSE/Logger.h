86f2a4678b1117eda23662900897e127540e6d9a
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2022-01-16 11:49:13 -0800

    fix compile error with new fmt

--- include/SKSE/Logger.h
+++ include/SKSE/Logger.h
@@ -10,9 +10,8 @@
 	{                                                                     \
 		a_func() = delete;                                                \
                                                                           \
-		template <class T>                                                \
 		explicit a_func(                                                  \
-			T&& a_fmt,                                                    \
+			fmt::format_string<Args...> a_fmt,                            \
 			Args&&... a_args,                                             \
 			std::source_location a_loc = std::source_location::current()) \
 		{                                                                 \
@@ -22,13 +21,13 @@
 					static_cast<int>(a_loc.line()),                       \
 					a_loc.function_name() },                              \
 				spdlog::level::a_type,                                    \
-				std::forward<T>(a_fmt),                                   \
+				a_fmt,                                                    \
 				std::forward<Args>(a_args)...);                           \
 		}                                                                 \
 	};                                                                    \
                                                                           \
-	template <class T, class... Args>                                     \
-	a_func(T&&, Args&&...) -> a_func<Args...>;
+	template <class... Args>                                              \
+	a_func(fmt::format_string<Args...>, Args&&...) -> a_func<Args...>;
 
 namespace SKSE::log
 {
