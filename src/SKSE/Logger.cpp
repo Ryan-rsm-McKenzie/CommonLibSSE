b3b092f9c0d5429fbf16e9d5f93720e9048a9ac3
Parapets <southwest85b@gmail.com>
2022-10-01 11:03:27 +0100

    Get log directory from game exe

--- src/SKSE/Logger.cpp
+++ src/SKSE/Logger.cpp
@@ -103,7 +103,9 @@ namespace SKSE
 			}
 
 			std::filesystem::path path = knownPath.get();
-			path /= "My Games/Skyrim Special Edition/SKSE"sv;
+			path /= "My Games"sv;
+			path /= *REL::Relocation<const char**>(REL::ID(380738)).get();
+			path /= "SKSE"sv;
 			return path;
 		}
 	}
