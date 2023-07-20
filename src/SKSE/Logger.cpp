3282565a2c86535036f5a556758fb1cf8ab85225
kkEngine <78795688+kkEngine@users.noreply.github.com>
2023-07-20 14:52:36 +0300

    - fixed namespace mismatching. In Logger.h "add_papyrus_sink" and "remove_papyrus_sink" functions in the SKSE::log namespace.

--- src/SKSE/Logger.cpp
+++ src/SKSE/Logger.cpp
@@ -59,37 +59,6 @@ namespace SKSE
 		};
 	}
 
-	void add_papyrus_sink(std::regex a_filter)
-	{
-		auto handler = Impl::LogEventHandler::GetSingleton();
-		handler->SetFilter(std::move(a_filter));
-
-		SKSE::RegisterForAPIInitEvent([]() {
-			auto papyrus = SKSE::GetPapyrusInterface();
-			if (papyrus) {
-				papyrus->Register([](RE::BSScript::IVirtualMachine* a_vm) {
-					auto handler = Impl::LogEventHandler::GetSingleton();
-					a_vm->RegisterForLogEvent(handler);
-					return true;
-				});
-			}
-		});
-	}
-
-	void remove_papyrus_sink()
-	{
-		SKSE::RegisterForAPIInitEvent([]() {
-			auto papyrus = SKSE::GetPapyrusInterface();
-			if (papyrus) {
-				papyrus->Register([](RE::BSScript::IVirtualMachine* a_vm) {
-					auto handler = Impl::LogEventHandler::GetSingleton();
-					a_vm->UnregisterForLogEvent(handler);
-					return true;
-				});
-			}
-		});
-	}
-
 	namespace log
 	{
 		std::optional<std::filesystem::path> log_directory()
@@ -107,6 +76,37 @@ namespace SKSE
 			path /= *REL::Relocation<const char**>(REL::ID(380738)).get();
 			path /= "SKSE"sv;
 			return path;
+		}
+
+		void add_papyrus_sink(std::regex a_filter)
+		{
+			auto handler = Impl::LogEventHandler::GetSingleton();
+			handler->SetFilter(std::move(a_filter));
+
+			SKSE::RegisterForAPIInitEvent([]() {
+				auto papyrus = SKSE::GetPapyrusInterface();
+				if (papyrus) {
+					papyrus->Register([](RE::BSScript::IVirtualMachine* a_vm) {
+						auto handler = Impl::LogEventHandler::GetSingleton();
+						a_vm->RegisterForLogEvent(handler);
+						return true;
+					});
+				}
+			});
+		}
+
+		void remove_papyrus_sink()
+		{
+			SKSE::RegisterForAPIInitEvent([]() {
+				auto papyrus = SKSE::GetPapyrusInterface();
+				if (papyrus) {
+					papyrus->Register([](RE::BSScript::IVirtualMachine* a_vm) {
+						auto handler = Impl::LogEventHandler::GetSingleton();
+						a_vm->UnregisterForLogEvent(handler);
+						return true;
+					});
+				}
+			});
 		}
 	}
 }
